bf2cf8ac1c836ac4ca56b35e9080e80b6c709fab
gongzhen <gongzhengz@gmail.com>
2015-09-14 17:29:01 -0700

    Added the sql database for Post

--- app/views/post.py
+++ app/views/post.py
@@ -1,12 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 post = Blueprint('post', __name__)
 
 # http://stackoverflow.com/questions/7974771/flask-blueprint-template-folder
-@post.route('/post/1')
-def pages():
-	return render_template('post/1.html')
+@post.route('/post/<int:id>')
+def pages(id):
+	post = Post.at(id).getone();
+	return render_template('post.html', post=post)
 
 @post.route('/post/write')
 def write():
-	return render_template('post/write.html')	
+	return render_template('post/write.html')
+
+@post.route('/post/create', methods=['POST'])
+def create():
+	return redirect(url_for('post', id=1))	
+
+@post.route('/post/edit/<int:id>')
+def edit(id):
+	return redirect(url_for('post/edit.html'))		
