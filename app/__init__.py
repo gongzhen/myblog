ba323f60da682e8c886d3ab24703a9b90ab6c2a8
gongzhen <gongzhengz@gmail.com>
2015-09-16 23:06:38 -0700

    Added the change for the post.html and create.html modified the view function

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
-from flask import Flask, request, render_template, redirect, url_for, request
+#!/usr/bin/env python
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+from flask import Flask, request, render_template, redirect, url_for, request
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db.init_app(app)
 
 from views.home import home as home_blueprint
 app.register_blueprint(home_blueprint)
