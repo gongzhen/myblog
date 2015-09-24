976ed25828a3b4e42810ae43f9fa1e3431b2f0ce
gongzhen <gongzhengz@gmail.com>
2015-09-24 16:14:37 -0700

    Changed the remember_me

--- app/forms.py
+++ app/forms.py
@@ -6,10 +6,6 @@ from wtforms import ValidationError
 class LoginForm(Form):
 	username = StringField('Username', validators=[Required(), Length(1, 64)])
 	password = PasswordField('Password', validators=[Required()])
-<<<<<<< HEAD
 	remeber_me = BooleanField('remember_me', default=False)
-=======
-	remeber_me = BooleanField('remeber_me', default=False)
->>>>>>> 63191c73a005cb04086134b607445927951da0d6
 	submit = SubmitField('Log In')
 	
