265b5ce507cc3298194910be0dd808778d080301
gongzhen <gongzhengz@gmail.com>
2015-09-27 00:55:24 -0700

    Modified css files.

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -20,6 +20,9 @@ $(document).ready(function () {
     // Will also work for relative and absolute hrefs
     $('ul.nav a').filter(function () {
         return this.href == url;
-    }).parent().addClass('active');
+    })
+    .parent()
+    .addClass('active');
+
 });
 
